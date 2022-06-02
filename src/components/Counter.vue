d394ba1079d3915d9c4d88748ced396b4c886efb
Jermaine Sepkitt <99402948+Sepkitt@users.noreply.github.com>
2022-06-02 12:17:50 +0200

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <button @click="increment">Counter</button>
+        <div class="counter">{{count}}</div>
+        <div class="content">{{arrayOfEmojis}}</div>
+
+
+    </div>
+</template>
+
+<!-- <script setup>
+import {ref} from 'vue'
+
+const count = ref(0)
+
+</script> -->
+
+<script >
+import { watch, ref, onMounted, computed } from 'vue'
+
+export default {
+
+    props: {
+        emoji: { type: String, default: '💩' }
+    },
+
+
+
+    setup(props) {
+        onMounted(() => {
+            console.log('Counter component has been mounted')
+        })
+        const count = ref(0)
+
+        const increment = () => count.value++
+
+        watch(count, current => {
+            if (current === 5) console.log('You Clicked 5 times')
+        })
+
+        const arrayOfEmojis = computed(() => 
+        Array.from(new Array(count.value), () => props.emoji).join(''))
+
+        return {
+            count,
+            increment,
+            arrayOfEmojis
+        }
+    }
+}
+
+</script>
+<style lang="scss" scoped>
+</style>
